01b722f7e5a0d2a8fd29bf895b9f368df87edcc8
himanshu gupta <himanshugupta@himanshus-MacBook-Air.local>
2024-12-21 08:57:03 +0530

    minor change

--- Backend/server.js
+++ Backend/server.js
@@ -6,8 +6,6 @@ const PORT = process.env.PORT || 4000;
 const connectDB = require("./db/db");
 connectDB();
 
-// console.log("BANSI");
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
